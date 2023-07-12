37c39d10b4d9a257606a9b02cbc055b54e1ad19d
Izar Castorina <izar@itcplatform.de>
2023-07-12 10:37:20 +0200

    Added new GetBackofficeToken method

--- Auth/Controllers/TokenController.cs
+++ Auth/Controllers/TokenController.cs
@@ -1,6 +1,9 @@
 using AuthenticationAPI.Services.Auth0.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Net;
+using System.Security;
 
 namespace Authentication.Controllers
 {
@@ -24,6 +27,35 @@ namespace Authentication.Controllers
         public async Task<IActionResult> GetToken([FromQuery] string username, [FromQuery] string password)
         {
             return Ok(await _publicTokenApi.GetTokenAsync(username, password));
+        }
+
+        [HttpPost]
+        [Route("GetBackofficeToken")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int)HttpStatusCode.TooManyRequests)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> GetBackofficeToken([FromForm] string username, [FromForm] string password)
+        {
+            string[] allowedRoles = { "admin", "backoffice" };
+
+            var tokenInfo = (System.Text.Json.JsonElement) await _publicTokenApi.GetTokenAsync(username, password);
+            
+            string? token = tokenInfo.GetProperty("access_token").ToString().Replace("bearer", "", StringComparison.OrdinalIgnoreCase)?.Trim();
+            if (string.IsNullOrEmpty(token)) return BadRequest("Token is empty, check credentials");
+            var handler = new JwtSecurityTokenHandler();
+            var jwtSecurityToken = handler.ReadJwtToken(token);
+
+            // Extrae los permisos del usuario desde el token
+            var permissions = jwtSecurityToken.Claims.Where(claim => claim.Type == "permissions").Select(e => e.Value.ToLowerInvariant()).ToArray();
+
+            // Mira si hay permisos en común entre los roles permitidos para la acción y los permisos del usuario
+            var hasAnyPermission = allowedRoles.Intersect(permissions).Any();
+
+            if (!hasAnyPermission) return Unauthorized();
+            else return Ok(tokenInfo);
+
         }
     }
 }

2b4b131b847fe3f3997724ee2a614b7ce08a1dae
Izar Castorina <izar@itcplatform.de>
2023-07-14 09:35:04 +0200

    Removed Debug and Release configs, in Auth: bugfix & added Swagger

--- Auth/Program.cs
+++ Auth/Program.cs
@@ -3,6 +3,7 @@ using Authentication.Models;
 using Authentication.Services.Auth0;
 using AuthenticationAPI.Services.Auth0.Interfaces;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,15 +23,41 @@ builder.Services.AddTransient<ApiErrorHandlingMiddleware>();
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "Authentication API", Version = "v1" });
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 // Add Auth0 configuration and services
 builder.Services.Configure<Auth0Settings>(builder.Configuration.GetSection("Auth0"));
 builder.Services.AddHttpClient<IPublicTokenAPI, PublicTokenAPI>();
-//builder.Services.AddHttpClient<IManagementTokenManager, ManagementTokenManager>();
+builder.Services.AddHttpClient<IManagementTokenManager, ManagementTokenManager>();
 builder.Services.AddHttpClient<IUsersAPI, UsersAPI>();
 builder.Services.AddHttpClient<IRolesAPI, RolesAPI>();
-//builder.Services.AddTransient<ManagementTokenManager>();
+builder.Services.AddTransient<ManagementTokenManager>();
 builder.Services.AddSingleton(provider =>
 {
     var auth0Settings = provider.GetRequiredService<IOptions<Auth0Settings>>().Value;

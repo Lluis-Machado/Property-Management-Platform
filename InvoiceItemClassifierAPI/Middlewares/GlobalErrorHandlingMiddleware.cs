80d6ff0fef8871ed9528d20a7b56991ce52a8087
lluis-itcp <82997570+lluis-itcp@users.noreply.github.com>
2023-07-21 11:10:00 +0200

    Error global handling middleware

---
+++ InvoiceItemClassifierAPI/Middlewares/GlobalErrorHandlingMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+
+namespace InvoiceItemClassifierAPI.Middlewares
+{
+    public class GlobalErrorHandlingMiddleware : IMiddleware
+    {
+        private readonly ILogger<GlobalErrorHandlingMiddleware> _logger;
+
+        public GlobalErrorHandlingMiddleware(ILogger<GlobalErrorHandlingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Internal exception occurred: {@Exception}", ex);
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                bool hasDeveloperPermission = context.User.Claims.Any(c => c.Type == "permissions" && c.Value == "admin");
+
+                if (hasDeveloperPermission)
+                {
+                    context.Response.ContentType = "text/plain";
+                    string responseContent = $"An error occurred: {ex.Message}\n\nStack Trace:\n{ex.StackTrace}";
+                    await context.Response.WriteAsync(responseContent);
+                }
+            }
+        }
+    }
+}

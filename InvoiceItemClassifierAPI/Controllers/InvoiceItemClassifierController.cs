5a9ba784d10140a38deff8de1faea81cb5f04949
lluis-itcp <82997570+lluis-itcp@users.noreply.github.com>
2023-07-21 10:59:34 +0200

    Invoice item classifier controller and model consumption

---
+++ InvoiceItemClassifierAPI/Controllers/InvoiceItemClassifierController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using static InvoiceLine;
+
+namespace InvoiceItemClassifierAPI.Controllers
+{
+
+    public class InvoiceItemClassifierController : Controller
+    {
+        // POST: Predict Invoice Item Categories
+        [HttpPost]
+        [Route("Predict")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+        public async Task<ActionResult<IEnumerable<ModelOutput>>> PredictInvoiceItemCategories([FromBody] List<ModelInput> modelInputs)
+        {
+            return Ok(PredictList(modelInputs));
+        }
+    }
+}

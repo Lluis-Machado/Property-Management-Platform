36b9b46834f04650302f4dcced7241ea0cd98a04
lluis-itcp <82997570+lluis-itcp@users.noreply.github.com>
2023-07-27 15:42:53 +0200

    Error fix countries contryId

--- CountriesAPI/Models/State.cs
+++ CountriesAPI/Models/State.cs
@@ -3,13 +3,12 @@
     public class State
     {
         public int Id { get; set; }
-        public string CountryCode { get; set; }
+        public int CountryId { get; set; }
         public string StateCode { get; set; }
         public string Name { get; set; }
 
         public State()
         {
-            CountryCode = string.Empty;
             StateCode = string.Empty;
             Name = string.Empty;
         }

937d6bddcbd8c33d7ffa2aa817d0d13291489479
Izar Castorina <izar@itcplatform.de>
2023-09-22 10:36:27 +0200

    Removed comment

--- Documents/Models/Archive.cs
+++ Documents/Models/Archive.cs
@@ -13,16 +13,6 @@ namespace DocumentsAPI.Models
             NONE
         }
 
-        // The archive Name is the display_name field in the metadata.
-        // The name of the container itself (FullArchiveId) will have this format:
-        /**
-         * For CONTACT archives:    ArchiveId = Contact.Id; FullArchiveId = "cont_{Contact.Id}"
-         * For COMPANY archives:    ArchiveId = Company.Id; FullArchiveId = "comp_{Company.Id}"
-         * For PROPERTY archives:   ArchiveId = Property.Id; FullArchiveId = "prop_{Contact.Id}"
-         * For archives not associated with any other object: ArchiveId = new Guid(); FullArchiveId = "generic_{ArchiveId}"
-         */
-
-
         public string? Name { get; set; }
         public ARCHIVE_TYPE ArchiveType { get; set; }
         public Guid? RelatedItemId { get; set; }
